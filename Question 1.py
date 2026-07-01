c5b40cabb589a9f85c43cdde4694632a5c065ab0
Manish Choudhary <152780586+Manishgit-hub@users.noreply.github.com>
2026-07-01 11:41:19 +0530

    changed
    Updated the get_dict_value function to remove unnecessary isinstance check and added test cases for student and teacher roll numbers.

--- Question 1.py
+++ Question 1.py
@@ -1,9 +1,25 @@
 def get_dict_value(dct, path):
-    keys = path.split('.')
+    keys = path.split(".")
     current = dct
+
     for key in keys:
-        if isinstance(current, dict) and key in current:
+        if key in current:
             current = current[key]
         else:
             return None
+
     return current
+
+
+dct = {
+    "student": {
+        "roll_number": "10",
+        "class": "1st"
+    },
+    "teacher": {
+        "school": "ABC"
+    }
+}
+
+print(get_dict_value(dct, "student.roll_number"))
+print(get_dict_value(dct, "teacher.roll_number"))
